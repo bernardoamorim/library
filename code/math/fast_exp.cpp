b76a39cc192bd4225b2ed9b88f632a2122006d57
Bernardo Amorim <bernardotamorim@gmail.com>
2020-04-17 16:32:26 -0300

    brushed matrix fexp

--- code/math/fast_exp.cpp
+++ code/math/fast_exp.cpp
@@ -26,58 +26,57 @@ ll fexp(ll x, ll y, ll m) { // recursive
 
 // (x^y) (mod m) in O(log(y)*(M.sz())^3)
 
-vector<vector<ll> > id(int sz) {
-	vector<vector<ll> > Mid(sz, vector<ll>(sz));
-	for(int i=0; i<sz; i++) {	
-		for(int j=0; j<sz; j++) {
-			if(i == j) Mid[i][j] = 1;
-			else Mid[i][j] = 0;
-		}
+typedef vector<vector<ll> > mat;
+
+mat idty(int a) {
+	mat id(a, vector<ll>(a,0));
+	for(int i=0; i < a; i++) {	
+		for(int j=0; j < a; j++) 
+			if(i == j) id[i][j] = 1;
 	}
-	return Mid;
+	return id;
 }
 
-vector<vector<ll> > mprod(vector<vector<ll> > a, vector<vector<ll> > b, ll m) {
-	int asz = (int) a.size();
-	int bsz = (int) b[0].size();
-	int csz = (int) a[0].size();	
+mat mmul(mat x, mat y, ll m) {
+	int a = (int) x.size();
+	int b = (int) y[0].size();
+	int c = (int) x[0].size();	
 
-	vector<vector<ll> > ret(asz, vector<ll>(bsz));
+	mat z(a, vector<ll>(b,0));
 	
-	for(int i=0; i < asz; i++) {
-		for(int j=0; j < bsz; j++) {
-			ll aux = 0;
-			for(int k=0; k < csz; k++) {
-				aux = (aux + a[i][k]*b[k][j]) % m;
+	for(int i=0; i < a; i++) {
+		for(int j=0; j < b; j++) {
+			for(int k=0; k < c; k++) {
+				z[i][j] += x[i][k]*y[k][j];
+				z[i][j] %= m;	
 			}	
-			ret[i][j] = aux;
 		}	
 	}
 
-	return ret;
+	return z;
 }
 
-vector<vector<ll> > mfexp(vector<vector<ll> > x, ll y, ll m) { // iterative
+mat mfexp(mat x, ll y, ll m) { // iterative
 	int sz = (int) x.size();
 	
-	vector<vector<ll> > ret(sz,vector<ll>(sz));
-	ret = id(sz);
+	mat z(sz,vector<ll>(sz));
+	z = idty(sz);
 
 	while (y) {
-		if (y & 1) ret = mprod(ret,x,m);
+		if (y & 1) z = mmul(z,x,m);
 		y >>= 1;
-		x = mprod(x,x,m);
+		x = mmul(x,x,m);
 	}
 
-	return ret;
+	return z;
 }
 
-vector<vector<ll> > mfexp(vector<vector<ll> > x, ll y, ll m) { // recursive
+mat mfexp(mat x, ll y, ll m) { // recursive
 	
 	if (!y) return id((int) x.size());
 
-	vector<vector<ll> > ans = mfexp(mprod(x,x,m), y/2, m);
+	mat z = mfexp(mmul(x,x,m), y/2, m);
 	
-	if(y % 2 == 0) return ans;
-	else return mprod(x,ans,m);
+	if(y % 2 == 0) return z;
+	else return mmul(x,z,m);
 }
