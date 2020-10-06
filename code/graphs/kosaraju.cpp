8a83bda482949c6b46eeed9d39d266a44fce6126
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-06 09:05:03 -0300

    fix identation kosaraju

--- code/graphs/kosaraju.cpp
+++ code/graphs/kosaraju.cpp
@@ -23,11 +23,11 @@ pair<int,vector<int>> kosaraju(int n) {
         for(int u : gt[v]) if(!vis[u]) dfst(u,c);
     };
  
- 	fill(vis.begin(), vis.end(),0);
+	fill(vis.begin(), vis.end(),0);
     for(int i=0; i<n; i++) if(!vis[i]) dfs(i);
     
     int ncomps = 0;
-    fill(vis.begin(),vis.end(),0);
+	fill(vis.begin(),vis.end(),0);
 	for(int u : to_look) if(!vis[u])
 		dfst(u,u), ncomps++;
  
