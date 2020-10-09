311dd1d35f61ee755bffabc08a731fa8912f05f9
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-09 12:29:14 -0300

    add modular inverse

---
+++ code/math/modular_inverse.cpp
@@ -0,0 +1,9 @@
+// Modular Inverse
+//
+// Compute inverse of a mod b
+// only guaranteed to exist if a and b are coprimes
+// If b is prime inv(a, b) = a^(b-2)
+
+ll inv(ll a, ll b) {
+	return a > 1 ? b - inv(b%a, a)*b/a : 1;
+}
