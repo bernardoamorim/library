4634f525132533ce1569b84a1b2960ba5b6cf731
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-09 12:32:13 -0300

    add complexity modular inverse

--- code/math/modular_inverse.cpp
+++ code/math/modular_inverse.cpp
@@ -3,6 +3,8 @@
 // Compute inverse of a mod b
 // only guaranteed to exist if a and b are coprimes
 // If b is prime inv(a, b) = a^(b-2)
+//
+// O(log(m))
 
 ll inv(ll a, ll b) {
 	return a > 1 ? b - inv(b%a, a)*b/a : 1;
