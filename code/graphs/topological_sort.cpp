7b71d4979747814ce04e17e92a46cf02a8ed6f84
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-09 12:42:56 -0300

    simplifying lambda in topological sort

--- code/graphs/topological_sort.cpp
+++ code/graphs/topological_sort.cpp
@@ -10,15 +10,15 @@ vector<int> g[MAX];
 vector<int> topo_sort(int n) {
 	vector<int> ret(n), vis(n);
 	
-	int pos = n-1;
+	int it = n-1;
 	bool dag = true;
-	function<void(int)> dfs = [&vis,&ret,&dag,&pos,&dfs] (int v) {
+	function<void(int)> dfs = [&] (int v) {
 		vis[v] = 1;
 		for(int u : g[v]) {
 			if(vis[u] == 1) dag = false;
 			else if(!vis[u]) dfs(u);
 		}
-		ret[pos--] = v, vis[v] = 2;
+		ret[it--] = v, vis[v] = 2;
 	};
 
 	for(int i=0; i<n; i++) if(!vis[i]) dfs(i);
