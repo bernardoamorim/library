fcf922569019a9478c4fec154d7fbe60ad5cf0d0
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-12 11:56:40 -0300

    add submasks

---
+++ code/problems/submasks.cpp
@@ -0,0 +1,10 @@
+// Submasks
+//
+// Iterates in all submasks of mask
+// O(3^n)
+
+for(int mask = 0; mask < (1 << n); mask++) {
+	for (int smask = (mask-1)&mask; smask > 0; smask = (smask-1)&mask) {
+		
+	}
+}
