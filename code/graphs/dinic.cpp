cd8bce824369fe445cba822fdb62ca188bc5c52d
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-12 16:02:49 -0300

    shortening dinic and making it a class

--- code/graphs/dinic.cpp
+++ code/graphs/dinic.cpp
@@ -5,57 +5,54 @@
 // O(min(m * max_flow, n^2 m))
 // Graph with capacities 1 -> O(sqrt(n)*m)
 
-struct dinic { 
+class dinic {
+private:
 	struct edge {
-		int to, idx, cap; // idx is even if original and odd if residual
-		edge (int to_, int idx_, int cap_) : to(to_), idx(idx_), cap(cap_) {}
+		int to, id, cap; // id is even if original and odd if residual
+		edge(int to_, int id_, int cap_) : to(to_), id(id_), cap(cap_) {}
 	};
-
-	int sz;
 	vector<vector<edge>> g;
-	vector<int> edg_flow, level, iter; 
-	dinic (int sz_) : sz(sz_), g(sz_), level(sz_), iter(sz_) {}
+	vector<int> flow, lvl, it;
+
+	int dfs(int v, int flow_aug, int t) {
+		if(v == t) return flow_aug;
+		for(int& i = it[v]; i < g[v].size(); i++) {
+			edge e = g[v][i];
+			int left = e.cap - flow[e.id];
+			if(lvl[e.to] != lvl[v] + 1 or !left) continue;
+			int aug = dfs(e.to, min(flow_aug, left), t);
+			if(!aug) continue;
+			flow[e.id] += aug, flow[e.id^1] -= aug;
+			return aug;
+		}
+		return 0;
+	}
+	int bfs(int s, int t) {
+		fill(lvl.begin(), lvl.end(), -1);
+		queue<int> q; q.push(s);
+		lvl[s] = 0;
+		while(q.size()) {
+			int u = q.front(); q.pop();
+			for(auto e : g[u]) { 
+				if(e.cap-flow[e.id] > 0 and lvl[e.to] == -1)
+					lvl[e.to] = lvl[u] + 1, q.push(e.to);
+			}
+		}
+		return lvl[t] != -1;
+	}
+public:
+	dinic(int sz) : g(sz), lvl(sz), it(sz) {}
 
 	void add_edge(int u, int v, int cap) {
-		g[u].push_back(edge(v, edg_flow.size(), cap));
-		edg_flow.push_back(0);
-		g[v].push_back(edge(u, edg_flow.size(), cap));
-		edg_flow.push_back(cap);
+		g[u].push_back(edge(v, flow.size(), cap)); flow.push_back(0);
+		g[v].push_back(edge(u, flow.size(), cap)); flow.push_back(cap);
 	}
-
 	int max_flow(int s, int t) {
-		function<int(int,int)> dfs = [&] (int v, int flow_aug) {
-			if(v == t) return flow_aug;
-			for(int& i = iter[v]; i < g[v].size(); i++) {
-				edge e = g[v][i];
-				int left = e.cap - edg_flow[e.idx], pushed = 0;
-				if(level[e.to] != level[v] + 1 or left == 0) continue;
-				pushed = dfs(e.to, min(flow_aug, left));
-				if(!pushed) continue;
-				edg_flow[e.idx] += pushed, edg_flow[e.idx^1] -= pushed;
-				return pushed;
-			}
-			return 0;
-		};
-		auto bfs = [&] () {
-			fill(level.begin(), level.end(), -1);
-			queue<int> q; q.push(s);
-			level[s] = 0;
-			while(q.size()) {
-				int u = q.front(); q.pop();
-				for(auto e : g[u]) if(e.cap - edg_flow[e.idx] > 0 and level[e.to] == -1) {
-					level[e.to] = level[u] + 1;
-					q.push(e.to);
-				}
-			}
-			return level[t] != -1;
-		};
-
 		int max_flow = 0;
-		while(bfs()) {
-			fill(iter.begin(), iter.end(), 0);
-			while(int aug = dfs(s,INF)) max_flow += aug;
+		while(bfs(s,t)) {
+			fill(it.begin(), it.end(), 0);
+			while(int aug = dfs(s,INF,t)) max_flow += aug;
 		}
-		return max_flow;		
+		return max_flow;	
 	}
 };
