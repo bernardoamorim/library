e433c3ee418fff1f5112c150b4634bbe40fe35e8
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-12 21:48:15 -0300

    dsu now is a struct

--- code/structures/dsu.cpp
+++ code/structures/dsu.cpp
@@ -4,17 +4,17 @@
 //
 // O(a(n)) ~= O(1)
 
-int id[MAX], sz[MAX];
-
-void dsu_build(int n) { for(int i=0; i<n; i++) sz[i] = 1, id[i] = i; }
-
-int find(int a) { return id[a] = a == id[a] ? a : find(id[a]); }
-
-void unite(int a, int b) {
-	a = find(a), b = find(b);
-	if(a == b) return;
-	if(sz[a] < sz[b]) swap(a,b);
-
-	sz[a] += sz[b];
-	id[b] = a;
-}
+struct dsu {
+	vector<int> id, sz;
+	dsu(int sz_) : id(sz_), sz(sz_,1) { iota(id.begin(), id.end(),0); }
+	
+	int find(int a) { return id[a] = a == id[a] ? a : find(id[a]); }
+	
+	void unite(int a, int b) {
+		a = find(a), b = find(b);
+		if(a == b) return;
+		if(sz[a] < sz[b]) swap(a,b);
+		sz[a] += sz[b];
+		id[b] = a;
+	}
+};
