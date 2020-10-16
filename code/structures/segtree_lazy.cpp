c3cf36cab86bf90f5af76d6acf44a923fa1dd9c4
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-15 23:00:34 -0300

    add lazy_segtree modifiable

--- code/structures/segtree_lazy.cpp
+++ code/structures/segtree_lazy.cpp
@@ -1,73 +1,83 @@
 // SegTree with Lazy Propagation
 //
-// Made to be modified
-// S is seg type and T is lazy tipe
-// apply(s,t,l,r) apply update s to a node with value t from l to r
-// upd_lazy(s1,s2) update s2 based on s1
+// Made to be modified, time will tell if doing this way is worth it
 //
-// query: sum [a, b]
-// update: summates x im each element in [a, b]
-//
+// Assuming ap, merge and comp are O(1)
 // build - O(n)
 // query - O(log(n))
 // update - O(log(n))
 
-template<typename T, typename S> class segtree {
-private:
-	int sz;
-	vector<T> seg;
-	vector<S> lazy;
-	T id = 0; 
-	S lazy_id = 0; 
+// <seg_type, s_id, merge, lz_type, l_id, apply_lz_to_seg, comp_lz>
 
-	T merge(T t1, T t2) { return t1 + t2; }
-	T apply(S s, T t, int l, int r) { return s*(r-l+1) + t; }
-	S upd_lazy(S s1, S s2) { return s1 + s2; } 
+template<class S,S (*s_id)(),S (*merge)(S,S),class L,L (*l_id)(),S (*ap)(L,S),L (*comp)(L,L)>
+class lazy_segtree {
 public:
-	T build(vector<T>& v, int p, int l, int r) {
+	int sz;
+	vector<S> seg;
+	vector<L> lz;
+	S build(vector<S>& v, int p, int l, int r) {
 		if (l == r) return seg[p] = v[l];
 		int m = (l+r)/2;
-		return seg[p] = merge(build(v,2*p, l, m), build(v,2*p+1, m+1, r));
+		return seg[p] = merge(build(v,2*p,l,m),build(v,2*p+1,m+1,r));
 	}
-	void prop(int p, int l, int r) {
-		if(lazy[p] == lazy_id) return;
-		S s = lazy[p];
-		int m = (l+r)/2;
-		seg[2*p] = apply(s, seg[2*p], l, m);
-		seg[2*p+1] = apply(s, seg[2*p+1], m+1, r);
-		lazy[2*p] = upd_lazy(s, lazy[2*p]);
-		lazy[2*p+1] = upd_lazy(s, lazy[2*p+1]);
-		lazy[p] = lazy_id;
+	void prop(int p) {
+		if(lz[p] == l_id()) return;
+		L l = lz[p];
+		seg[2*p] = ap(l,seg[2*p]), seg[2*p+1] = ap(l,seg[2*p+1]);
+		lz[2*p] = comp(l,lz[2*p]), lz[2*p+1] = comp(l,lz[2*p+1]);
+		lz[p] = l_id();
 	}
-	T query(int s, int e) { return query(s, e, 1, 0, sz-1); }
-	T query(int s, int e, int p, int l, int r) {
-		if(e < l or r < s) return id;
+	S query(int s, int e, int p, int l, int r) {
+		if(e < l or r < s) return s_id();
 		if(s <= l and r <= e) return seg[p];
-		prop(p, l, r);
-		int m = (l+r)/2;
-		return merge(query(s, e, 2*p, l, m), query(s, e, 2*p+1, m+1, r));
+		prop(p); int m = (l+r)/2;
+		return merge(query(s,e,2*p,l,m), query(s,e,2*p+1,m+1,r));
 	}
-	T upd(int s, int e, S x) { return upd(s, e, x, 1, 0, sz-1); }
-	T upd(int s, int e, S x, int p, int l, int r) {
+	S update(int s, int e, L x, int p, int l, int r) {
 		if(e < l or r < s) return seg[p];
 		if(s <= l and r <= e) {
-			seg[p] = apply(x, seg[p], l, r);
-			lazy[p] = upd_lazy(x, lazy[p]);
-			return seg[p];
+			lz[p] = comp(x, lz[p]);
+			return seg[p] = ap(x, seg[p]);
 		}
-		prop(p, l, r);
-		int m = (l+r)/2;
-		return seg[p] = merge(upd(s, e, x, 2*p, l, m), upd(s, e, x, 2*p+1, m+1, r));
+		prop(p); int m = (l+r)/2;
+		return seg[p] = merge(update(s,e,x,2*p,l,m), update(s,e,x,2*p+1,m+1,r));
 	}
-	segtree(int sz_) : sz(sz_) {
-		seg.resize(4*sz, id);
-		lazy.resize(4*sz, lazy_id);
-	}
-	segtree(int sz_, vector<T> v) : sz(sz_) {
-		seg.resize(4*sz, id);
-		lazy.resize(4*sz, lazy_id);
+public:
+	S update(int s, int e, L x) { return update(s, e, x, 1, 0, sz-1); }
+	S query(int s, int e) { return query(s, e, 1, 0, sz-1); }
+	lazy_segtree() : lazy_segtree(0) {}
+	lazy_segtree(int sz_) : lazy_segtree(vector<S>(sz_)) {}
+	lazy_segtree(vector<S> v) {
+		sz = v.size();
+		seg = vector<S>(4*sz, s_id()); 
+		lz = vector<L>(4*sz, l_id());
 		build(v,1,0,sz-1);
 	}
 };
+
+// Example:
+// query: sum [a, b]
+// update: summates x im each element in [a, b]
+
+// Seg type (with empty constructor)
+struct node { ll sum = 0, sz = 1; };
+
+// Seg type identity element
+node s_id() { return node{0,0};}
+
+// Merge two segs
+node merge(node l, node r) { return node{l.sum+r.sum, l.sz+r.sz}; };
+
+// Update (lazy) type
+// ll
+
+// Update (lazy) identity element
+ll l_id() { return 0; }
+
+// Apply update to segtree
+node ap(ll v, node p) { return node{p.sum + v*p.sz, p.sz}; };
+
+// Compose two updates (left is the newer if it matters)
+ll comp(ll l, ll r) { return l+r; };
 
 // TO ADD LEFT AND RIGHT
