2477e32ac6ef11cff4fda5d42aff5546e20f62f5
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-20 14:05:49 -0300

    smaller variable names in trie

--- code/strings/trie.cpp
+++ code/strings/trie.cpp
@@ -1,58 +1,59 @@
 // Trie
-// trie T(alphabet size, initial char (i.e.'a' for lowercase letters)) 
+// trie T(alphabet size, initial char (i.e.'a' for lowercase letters))
+// be careful, alphabet size is the size from norm to the last char used
 //
 // insert, erase, count and find: O(|s|)
 // prefix(s) returns all strings that have s as prefix excluding s itself
 
 struct trie {
-	vector<vector<int>> tree;
+	vector<vector<int>> to;
 	vector<int> end;
 	int sigma; char norm;
 	trie(int sigma_, char norm_) : sigma(sigma_), norm(norm_) {
-		tree.push_back(vector<int>(sigma,-1));
-		end.push_back(0);
+		to = {vector<int>(sigma,-1)};
+		end = {0};
 	}
 	void insert(string s) {
-		int node = 0;
+		int v = 0;
 		for(auto c : s) {
-			int& nxt = tree[node][c-norm];
+			int& nxt = to[v][c-norm];
 			if(nxt == -1) {
-				nxt = tree.size();
-				tree.push_back(vector<int>(sigma,-1));
+				nxt = to.size();
+				to.push_back(vector<int>(sigma,-1));
 				end.push_back(0);
 			}
-			node = nxt;
+			v = nxt;
 		}
-		end[node]++;
+		end[v]++;
 	}
 	int find(string s) {
-		int node = 0;
+		int v = 0;
 		for(auto c : s) {
-			if(tree[node][c-norm] == -1) return -1;
-			node = tree[node][c-norm];
+			if(to[v][c-norm] == -1) return -1;
+			v = to[v][c-norm];
 		}
-		return node;
+		return v;
 	}
 	int count(string s) {
-		int node = find(s);
-		return node == -1 ? 0 : end[node];
+		int v = find(s);
+		return v == -1 ? 0 : end[v];
 	}
 	void erase(string s) {
-		int snode = find(s);
-		if(snode != -1) end[snode]--;
+		int vs = find(s);
+		if(vs != -1) end[vs]--;
 	}
 	vector<string> prefix(string s) {
 		vector<string> ret;	
-		int snode = find(s);
-		function<void(int)> dfs = [&] (int tnode) {
-			if(end[tnode] and tnode != snode) ret.push_back(s);
-			for(int i = 0; i < sigma; i++) if(tree[tnode][i] != -1) {
-				s += norm + i;
-				dfs(tree[tnode][i]);
+		int vs = find(s);
+		function<void(int)> dfs = [&] (int vt) {
+			if(end[vt] and vt != vs) ret.push_back(s);
+			for(int c = 0; c < sigma; c++) if(to[vt][c] != -1) {
+				s += norm + c;
+				dfs(to[vt][c]);
 				s.pop_back();
 			}
 		};
-		if(snode != -1) dfs(snode);
+		if(vs != -1) dfs(vs);
 		return ret;
 	}
 };
