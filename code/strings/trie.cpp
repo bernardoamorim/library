d876ca809dcad698d0ee0416f42835af29c18215
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-23 13:38:25 -0300

    Emanuel changes in trie

--- code/strings/trie.cpp
+++ code/strings/trie.cpp
@@ -1,59 +1,54 @@
 // Trie
-// trie T(alphabet size, initial char (i.e.'a' for lowercase letters))
-// be careful, alphabet size is the size from norm to the last char used
 //
-// insert, erase, count and find: O(|s|)
-// prefix(s) returns all strings that have s as prefix excluding s itself
+// trie T() constructs a trie with the lowercase letters as alphabet
+// trie T(sigma, smallest char in ASCII)
+// 
+// insert - O(|s|*sigma)
+// erase - O(|s|)
+// find(s) returns the position os s in the vector and 0 if does not find O(|s|)
+// count_pref(s) numbers of strings that have s as prefix O(|s|)
+//
+// Does not work for empty strings
 
 struct trie {
 	vector<vector<int>> to;
-	vector<int> end;
+	vector<int> end, pref;
 	int sigma; char norm;
-	trie(int sigma_, char norm_) : sigma(sigma_), norm(norm_) {
-		to = {vector<int>(sigma,-1)};
-		end = {0};
+	trie(int sigma_=26, char norm_='a') : sigma(sigma_), norm(norm_) {
+		to = {vector<int>(sigma)};
+		end = {0}, pref = {0};
 	}
 	void insert(string s) {
-		int v = 0;
+		int x = 0;
 		for(auto c : s) {
-			int& nxt = to[v][c-norm];
-			if(nxt == -1) {
+			int &nxt = to[x][c-norm];
+			if(!nxt) {
 				nxt = to.size();
-				to.push_back(vector<int>(sigma,-1));
-				end.push_back(0);
+				to.push_back(vector<int>(sigma));
+				end.push_back(0), pref.push_back(0);
 			}
-			v = nxt;
+			x = nxt, pref[x]++;
 		}
-		end[v]++;
-	}
-	int find(string s) {
-		int v = 0;
-		for(auto c : s) {
-			if(to[v][c-norm] == -1) return -1;
-			v = to[v][c-norm];
-		}
-		return v;
-	}
-	int count(string s) {
-		int v = find(s);
-		return v == -1 ? 0 : end[v];
+		end[x]++;
 	}
 	void erase(string s) {
-		int vs = find(s);
-		if(vs != -1) end[vs]--;
+		int x = 0;
+		for(char c : s) {
+			int &nxt = to[x][c-norm];
+			x = nxt, pref[x]--;
+			if(!pref[x]) nxt = 0;
+		}
+		end[x]--;
 	}
-	vector<string> prefix(string s) {
-		vector<string> ret;	
-		int vs = find(s);
-		function<void(int)> dfs = [&] (int vt) {
-			if(end[vt] and vt != vs) ret.push_back(s);
-			for(int c = 0; c < sigma; c++) if(to[vt][c] != -1) {
-				s += norm + c;
-				dfs(to[vt][c]);
-				s.pop_back();
-			}
-		};
-		if(vs != -1) dfs(vs);
-		return ret;
+	int find(string s) {
+		int x = 0;
+		for(auto c : s) {
+			x = to[x][c-norm];
+			if(!x) return 0;
+		}
+		return x;
+	}
+	int count_pref(string s) {
+		return pref[find(s)];
 	}
 };
