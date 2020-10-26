50ba6cb0b1d1286f643549b2f7a70484a657515c
Bernardo Amorim <bernardotamorim@gmail.com>
2020-10-26 20:13:49 -0300

    now a good segment tree

--- code/structures/segtree_lazy.cpp
+++ code/structures/segtree_lazy.cpp
@@ -1,83 +1,64 @@
 // SegTree with Lazy Propagation
 //
-// Made to be modified, time will tell if doing this way is worth it
+// lazy_segtree<seg type, lazy type>
+// you have to change things inside seg anyway, but I think this way is better
+// 
+// Sum of range implement (lazy_segtree<pair<ll,int>, int>)
 //
 // Assuming ap, merge and comp are O(1)
 // build - O(n)
 // query - O(log(n))
 // update - O(log(n))
 
-// <seg_type, s_id, merge, lz_type, l_id, apply_lz_to_seg, comp_lz>
-
-template<class S,S (*s_id)(),S (*merge)(S,S),class L,L (*l_id)(),S (*ap)(L,S),L (*comp)(L,L)>
-class lazy_segtree {
-public:
+template<typename T,typename U> struct lazy_segtree {
 	int sz;
-	vector<S> seg;
-	vector<L> lz;
-	S build(vector<S>& v, int p, int l, int r) {
+	T s_id = {0,0};
+	U l_id = 0;
+	vector<T> seg, v;
+	vector<U> lazy;
+	
+	T merge(T s1, T s2) { return {s1.first + s2.first, s1.second + s2.second}; }
+	T apply(U x, T s) { return {s.first + x * s.second, s.second}; }
+	U compose(U x1, U x2) { return x1 + x2; }
+	
+	T build(int p, int l, int r) {
 		if (l == r) return seg[p] = v[l];
 		int m = (l+r)/2;
-		return seg[p] = merge(build(v,2*p,l,m),build(v,2*p+1,m+1,r));
+		return seg[p] = merge(build(2*p,l,m), build(2*p+1,m+1,r));
 	}
 	void prop(int p) {
-		if(lz[p] == l_id()) return;
-		L l = lz[p];
-		seg[2*p] = ap(l,seg[2*p]), seg[2*p+1] = ap(l,seg[2*p+1]);
-		lz[2*p] = comp(l,lz[2*p]), lz[2*p+1] = comp(l,lz[2*p+1]);
-		lz[p] = l_id();
+		U x = lazy[p];
+		if(x == l_id) return;
+		seg[2*p] = apply(x,seg[2*p]);
+		seg[2*p+1] = apply(x,seg[2*p+1]);
+		lazy[2*p] = compose(x,lazy[2*p]);
+		lazy[2*p+1] = compose(x,lazy[2*p+1]);
+		lazy[p] = l_id;
 	}
-	S query(int s, int e, int p, int l, int r) {
-		if(e < l or r < s) return s_id();
+	T query(int s, int e, int p, int l, int r) {
+		if(e < l or r < s) return s_id;
 		if(s <= l and r <= e) return seg[p];
-		prop(p); int m = (l+r)/2;
+		prop(p); 
+		int m = (l+r)/2;
 		return merge(query(s,e,2*p,l,m), query(s,e,2*p+1,m+1,r));
 	}
-	S update(int s, int e, L x, int p, int l, int r) {
+	T update(int s, int e, U x, int p, int l, int r) {
 		if(e < l or r < s) return seg[p];
 		if(s <= l and r <= e) {
-			lz[p] = comp(x, lz[p]);
-			return seg[p] = ap(x, seg[p]);
+			lazy[p] = compose(x, lazy[p]);
+			return seg[p] = apply(x, seg[p]);
 		}
-		prop(p); int m = (l+r)/2;
+		prop(p); 
+		int m = (l+r)/2;
 		return seg[p] = merge(update(s,e,x,2*p,l,m), update(s,e,x,2*p+1,m+1,r));
 	}
-public:
-	S update(int s, int e, L x) { return update(s, e, x, 1, 0, sz-1); }
-	S query(int s, int e) { return query(s, e, 1, 0, sz-1); }
-	lazy_segtree() : lazy_segtree(0) {}
-	lazy_segtree(int sz_) : lazy_segtree(vector<S>(sz_)) {}
-	lazy_segtree(vector<S> v) {
-		sz = v.size();
-		seg = vector<S>(4*sz, s_id()); 
-		lz = vector<L>(4*sz, l_id());
-		build(v,1,0,sz-1);
+
+	lazy_segtree(int sz_, vector<T> v_={}) : sz(sz_), seg(4*sz_, s_id), lazy(4*sz_, l_id) {
+		v = (v_.empty() ? vector<T>(sz, {0,1}) : v_); // be careful with initial value
+		build(1,0,sz-1);
 	}
+	T update(int s, int e, U x) { return update(s, e, x, 1, 0, sz-1); }
+	T query(int s, int e) { return query(s, e, 1, 0, sz-1); }
 };
-
-// Example:
-// query: sum [a, b]
-// update: summates x im each element in [a, b]
-
-// Seg type (with empty constructor)
-struct node { ll sum = 0, sz = 1; };
-
-// Seg type identity element
-node s_id() { return node{0,0};}
-
-// Merge two segs
-node merge(node l, node r) { return node{l.sum+r.sum, l.sz+r.sz}; };
-
-// Update (lazy) type
-// ll
-
-// Update (lazy) identity element
-ll l_id() { return 0; }
-
-// Apply update to segtree
-node ap(ll v, node p) { return node{p.sum + v*p.sz, p.sz}; };
-
-// Compose two updates (left is the newer if it matters)
-ll comp(ll l, ll r) { return l+r; };
 
 // TO ADD LEFT AND RIGHT
