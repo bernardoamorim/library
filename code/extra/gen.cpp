98c13a4e042e18c8b9a1c29bb09b88913f12332e
Bernardo Amorim <bernardotamorim@gmail.com>
2020-11-11 09:43:05 -0300

    stress.sh and gen.cpp

---
+++ code/extra/gen.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int rand(int a, int b) { return rand() % (b-a+1) + a; }
+
+int main(int argc, char** argv){ _
+	srand(atoi(argv[1]));
+	
+
+	return 0;
+}
---
+++ code/extra/stress.sh
@@ -0,0 +1,20 @@
+# a is the name of the tested code, a2 is brute force code and gen is the generator
+# to run do: bash stress.sh 
+# to create executable do: ch +x stress.sh
+
+make a a2 gen || exit 1
+for ((i = 1; ; i++)) do
+	./gen $i > in
+	./a < in > out
+	./a2 < in > out2
+	if (! cmp -s out out2) then
+		echo "--> in:"
+		cat in
+		echo "--> out1:"
+		cat out
+		echo "--> out22:"
+		cat out2
+		break;
+	fi
+	echo $i
+done
