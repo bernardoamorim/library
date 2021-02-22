ce2cb31955d2d1805da248aff1abc6bcde03b86b
Bernardo Amorim <bernardotamorim@gmail.com>
2021-02-22 20:16:42 -0300

    small change in fexp

--- code/math/fast_exp.cpp
+++ code/math/fast_exp.cpp
@@ -5,19 +5,10 @@
 ll fexp(ll x, ll y, ll m) { // iterative
 	ll r = 1;
 	x %= m;
-	while (y) {
-		if (y & 1) r = (r*x) % m;
+	while(y > 0) {
+		if(y & 1) r = (r*x) % m;
 		y >>= 1;
 		x = (x*x) % m;
 	}
 	return r;
-}
-
-ll fexp(ll x, ll y, ll m) { // recursive
-	x %= m;
-	if (!y) return 1;
-	ll ans = fexp(x*x % m, y/2, m);
-	
-	if(y % 2 == 0) return ans;
-	else return x*ans % m;
 }
