18e33b92e14cd2c820f22baff0da639ff8d2d75d
Bernardo Amorim <bernardotamorim@gmail.com>
2021-02-22 10:37:54 -0300

    even better tree isomorphism

--- code/graphs/tree_isomorphism.cpp
+++ code/graphs/tree_isomorphism.cpp
@@ -1,7 +1,7 @@
 // Tree isomorphism
 // 
-// thash() returns the hash of the tree. 
-// Two trees are isomorph if their hash is the same.
+// thash() returns the hash of the tree. (using as special vertices the centroids)
+// Two trees are isomorph if their hash is the same. 
 // 
 // Complexity: O(|V|.log(|V|))
 
@@ -10,23 +10,18 @@ map<vector<int>, int> mphash;
 struct tree {
 	int n;
 	vector<vector<int>> g;
-	vector<int> sz;
+	vector<int> sz, cs;
 
 	tree(int n_) : n(n_), g(n_), sz(n_) {}
 
-	vector<int> centroids(int root, int ssz) {
-		vector<int> c;
-		function<void(int,int)> dfs = [&] (int v, int p) {
-			sz[v] = 1;
-			bool cent = true;
-			for(int u : g[v]) if(u != p) {
-				dfs(u, v), sz[v] += sz[u];
-				if(sz[u] > ssz/2) cent = false;
-			}
-			if(cent and ssz - sz[v] <= ssz/2) c.push_back(v);
-		};
-		dfs(root, -1);
-		return c;
+	void dfs_centroid(int v, int p) {
+		sz[v] = 1;
+		bool cent = true;
+		for(int u : g[v]) if(u != p) {
+			dfs_centroid(u, v), sz[v] += sz[u];
+			if(sz[u] > n/2) cent = false;
+		}
+		if(cent and n - sz[v] <= n/2) cs.push_back(v);
 	}
 	int fhash(int v, int p) {
 		vector<int> h;
@@ -37,7 +32,8 @@ struct tree {
 		return mphash[h];
 	}
 	ll thash() {
-		vector<int> cs = centroids(0, n);
+		cs.clear();
+		dfs_centroid(0, -1);
 		if(cs.size() == 1) return fhash(cs[0], -1);
 		ll h1 = fhash(cs[0], cs[1]), h2 = fhash(cs[1], cs[0]);
 		return (min(h1, h2) << 30) + max(h1, h2);
