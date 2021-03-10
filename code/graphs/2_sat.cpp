5017c7c880ff54945a7a9f5f4982b00e51fa21ed
Bernardo Amorim <bernardotamorim@gmail.com>
2021-03-10 09:18:39 -0300

    style

--- code/graphs/2_sat.cpp
+++ code/graphs/2_sat.cpp
@@ -6,7 +6,7 @@
 // O(|V| + |E|), where |V| = number of variables and |E| is the 
 // number of logical statements.
 
-vector<int> g[2 * MAX], gt[2 * MAX];
+vector<int> g[2*MAX], gt[2*MAX];
 
 // pair<int,vector<int>> kosaraju(int n) returns {ncomps, comp};
 // In comp, if there is a path from comp[i] to comp[j], comp[i] > comp[j]
@@ -18,14 +18,10 @@ struct _2sat {
 
 	int no(int id) { return (id + n) % (2 * n); }
 
-	void add(int x, int y) {
+	void imply(int x, int y) {
 		g[x].push_back(y), g[no(y)].push_back(no(x));
 		gt[y].push_back(x), gt[no(x)].push_back(no(y));
 	}
-
-	void add_or(int x, int y) { add(no(x), y); }
-
-	void add_imply(int x, int y) { add(x, y); }
 
 	pair<bool,vector<bool>> solve() {
 		vector<bool> assignment(n); // to be tested
@@ -36,7 +32,7 @@ struct _2sat {
 			if(comp[i] == comp[no(i)]) sat = false;
 			else assignment[i] = comp[i] < comp[no(i)];
 		}
-		if(!sat) assignment.clear();
+		if(not sat) assignment.clear();
 		return {sat, assignment};
 	}
 };
