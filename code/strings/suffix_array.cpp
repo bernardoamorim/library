a8c73af67ac2ce01e8d07145582ca97df312115e
Bernardo Amorim <bernardotamorim@gmail.com>
2021-05-04 13:57:57 -0300

    consistency

--- code/strings/suffix_array.cpp
+++ code/strings/suffix_array.cpp
@@ -12,19 +12,22 @@ vector<int> suffix_array(string s) {
 	
 	for(int i = 0; i < n; i++) sa[i] = i, p[i] = s[i];
 	
-	while(k < n) {
+	while (k < n) {
 		vector<int> b(N), nsa(n), np(n);
-		for(int i = 0; i < n; i++) b[p[(i-k+n) % n]]++;
-		for(int i = 1; i < N; i++) b[i] += b[i-1];
+		for (int i = 0; i < n; i++) 
+			b[p[(i-k+n) % n]]++;
+		for (int i = 1; i < N; i++) 
+			b[i] += b[i-1];
 		
-		for(int i = n-1; i >= 0; i--) 
+		for (int i = n-1; i >= 0; i--) 
 			nsa[--b[p[(sa[i]-k+n) % n]]] = (sa[i]-k+n) % n;
 		
 		sa = nsa, ct = 0;
 		pair<int,int> prev, cur;
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			cur = {p[sa[i]], p[(sa[i]+k) % n]};
-			if(i and prev != cur) ct++;
+			if (i and prev != cur) 
+				ct++;
 			np[sa[i]] = ct, prev = cur;
 		}
 		p = np, k = (k ? 2 * k : 1);
@@ -35,11 +38,15 @@ vector<int> suffix_array(string s) {
 vector<int> lcp(string s, vector<int> sa) {
 	int n = s.size(), k = 0;
 	vector<int> p(n), lcp(n);
-	for(int i = 0; i < n; i++) p[sa[i]] = i;
-	for(int i = 0; i < n - 1; i++) if(p[i]) {
-		int j = sa[p[i]-1];
-		while(s[i+k] == s[j+k]) k++;
-		lcp[p[i]] = k, k = max(k-1, 0);
+	for (int i = 0; i < n; i++) 
+		p[sa[i]] = i;
+	for (int i = 0; i < n - 1; i++) {
+		if (p[i]) {
+			int j = sa[p[i]-1];
+			while (s[i+k] == s[j+k]) 
+				k++;
+			lcp[p[i]] = k, k = max(k-1, 0);
+		}
 	}
-	return vector<int>(lcp.begin()+1,lcp.end());
+	return vector<int>(lcp.begin()+1, lcp.end());
 }
