36b2abf3e57b0066ab4770d65434e9fb16964f37
Bernardo Amorim <bernardotamorim@gmail.com>
2021-05-04 13:48:52 -0300

    Consistency in DSU

--- code/structures/dsu.cpp
+++ code/structures/dsu.cpp
@@ -4,17 +4,22 @@
 //
 // O(a(n)) ~= O(1)
 
-struct dsu {
+struct DSU {
 	vector<int> id, sz;
-	dsu(int sz_) : id(sz_), sz(sz_,1) { iota(id.begin(), id.end(),0); }
+	dsu(int sz_) : id(sz_), sz(sz_,1) { 
+		iota(id.begin(), id.end(),0); 
+	}
 	
-	int find(int a) { return id[a] = a == id[a] ? a : find(id[a]); }
+	int find(int a) { 
+		return id[a] = a == id[a] ? a : find(id[a]); 
+	}
 	
 	void unite(int a, int b) {
 		a = find(a), b = find(b);
-		if(a == b) return;
-		if(sz[a] < sz[b]) swap(a,b);
-		sz[a] += sz[b];
-		id[b] = a;
+		if(a == b) 
+			return;
+		if(sz[a] < sz[b]) 
+			swap(a,b);
+		sz[a] += sz[b], id[b] = a;
 	}
 };
