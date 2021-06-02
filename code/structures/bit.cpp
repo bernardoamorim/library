93d0fd171b3394ba3c3a2faf38bafcb4b1b14069
Bernardo Amorim <bernardotamorim@gmail.com>
2021-06-02 17:34:52 -0300

    bit 1d added

--- code/structures/bit.cpp
+++ code/structures/bit.cpp
@@ -1,55 +1,34 @@
-// BIT - Binary Indexed Tree (Fenwick Tree) with range update
+// BIT - Binary Indexed Tree (Fenwick Tree)
 //
 // all operations are 0-based
 // can build an empty one or based on a vector
 //
 // Complexity:
 // constructor: O(n)
-// add, pref, sum: O(log(n))
+// add, pref, query: O(log(n))
 
 struct Bit {
 	int n;
-	vector<ll> tree[2];
-	
-	Bit(int n_) : n(n_) { 
-		tree[0].resize(n+1), tree[1].resize(n+1); 
+	vector<ll> tree;
+
+	Bit(int n_) : n(n_), tree(n_ + 1) {}
+
+	void add(int x, int p) {
+		for (p++; p <= n; p += p & -p) tree[p] += x;
 	}
-	
-	Bit(vector<int> v) : n(v.size()) {
-		tree[0].resize(n+1), tree[1].resize(n+1);
-		for (int i = 1; i <= n; i++) {
-			tree[1][i] += v[i-1];
-			if (i + (i & -i) <= n) 
-				tree[1][i + (i & -i)] += tree[1][i];
-		}
-	}
-	
-	void add_(ll x, int p, int w) {
-		p++;
-		while (p <= n) 
-			tree[w][p] += x, p += p & -p;
-	}
-	
-	ll pref_(int p, int w) {
+	ll pref(int p) {
 		ll ret = 0;
-		p++;
-		while (p > 0) 
-			ret += tree[w][p], p -= p & -p;
+		for (p++; p > 0; p -= p & -p) ret += tree[p];
 		return ret;
 	}
-	
-	void add(ll x, int l, int r = -1) {
-		if (r == -1) 
-			r = l;
-		add_(x, l, 0), add_(-x, r+1, 0);
-		add_(-x*l, l, 1), add_(x*(r+1), r+1, 1);
-	}
-	
-	ll pref(int p) { 
-		return pref_(p,0) * (p+1) + pref_(p,1); 
-	}
-	
-	ll sum(int l, int r) { 
-		return pref(r) - pref(l-1); 
-	}
+	ll query(int l, int r) { return pref(r) - pref(l-1); }
 };
+
+/* linear build on a vector
+Bit(vector<int> v) : n(v.size()), tree(v.size() + 1) {
+	for (int i = 1; i <= n; i++) {
+		tree[i] += v[i-1];
+		if (i + (i & -i) <= n) tree[i + (i & -i)] += tree[i];
+	}
+}
+*/
