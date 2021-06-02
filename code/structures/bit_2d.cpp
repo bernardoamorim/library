6cb36453880a5422adcd14da44e4ca3d250418f9
Bernardo Amorim <bernardotamorim@gmail.com>
2021-06-02 17:35:09 -0300

    separeted bit 1d from 2d

---
+++ code/structures/bit_2d.cpp
@@ -0,0 +1,44 @@
+// BIT - Binary Indexed Tree (Fenwick Tree) with range update
+//
+// all operations are 0-based
+// can build an empty one or based on a vector
+//
+// Complexity:
+// constructor: O(n)
+// add, pref, sum: O(log(n))
+
+struct Bit_2D {
+	int n;
+	vector<ll> tree[2];
+	
+	Bit_2D(int n_) : n(n_) { 
+		tree[0].resize(n+1), tree[1].resize(n+1); 
+	}
+	
+	void add_(ll x, int p, int w) {
+		for (p++; p <= n; p += p & -p) tree[w][p] += x;
+	}
+	ll pref_(int p, int w) {
+		ll ret = 0;
+		for (p++; p > 0; p -= p & -p) ret += tree[w][p];
+		return ret;
+	}
+	void add(ll x, int l, int r = -1) {
+		if (r == -1) r = l;
+		add_(x, l, 0), add_(-x, r+1, 0);
+		add_(-x*l, l, 1), add_(x*(r+1), r+1, 1);
+	}
+	ll pref(int p) { return pref_(p,0) * (p+1) + pref_(p,1); }
+	ll query(int l, int r) { return pref(r) - pref(l-1); }
+};
+
+/* linear build on a vector
+Bit_2D(vector<int> v) : n(v.size()) {
+	tree[0].resize(n+1), tree[1].resize(n+1);
+	for (int i = 1; i <= n; i++) {
+		tree[1][i] += v[i-1];
+		if (i + (i & -i) <= n) 
+			tree[1][i + (i & -i)] += tree[1][i];
+	}
+}
+*/
