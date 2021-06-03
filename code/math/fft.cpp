96e26df16ad0f8f5713f976dcbab66fe9a17a8d5
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-06-02 21:17:22 -0300

    Update fft.cpp

--- code/math/fft.cpp
+++ code/math/fft.cpp
@@ -12,20 +12,17 @@ inline complex<ld> unity_root(int n, bool inv) { // unity_root(n)^n = (e^(2.pi.i
 
 template<typename T> void fft(vector<T>& a, bool inv) {
 	for(int i = 1, j = 0; i < a.size(); i++) { // reverse bit order
-		int bit = a.size()/2;
-		while (j & bit)
-			j ^= bit, bit /= 2;
+		int bit = a.size() / 2;
+		while (j & bit) j ^= bit, bit /= 2;
 		j ^= bit;
-		if (i < j)
-			swap(a[i], a[j]);
+		if (i < j) swap(a[i], a[j]);
 	}
 
 	for (int n = 2; n <= a.size(); n *= 2) {
 		vector<T> roots(n/2);
 		T omega = unity_root(n, inv);
 		roots[0] = 1;
-		for (int i = 1; i < n/2; i++)
-			roots[i] = roots[i-1] * omega;
+		for (int i = 1; i < n/2; i++) roots[i] = roots[i-1] * omega;
 		for (int i = 0; i < a.size(); i += n) {
 			int l = i, r = i + n/2;
 			for (int j = 0; j < n/2; j++, l++, r++) {
@@ -43,12 +40,10 @@ template<typename T> vector<T> convolution(vector<T> A, vector<T> B) {
 	fft(A, false), fft(B, false);
 	vector<T> C(n);
 
-	for (int i = 0; i < n; i++)
-		C[i] = A[i] * B[i];
+	for (int i = 0; i < n; i++) C[i] = A[i] * B[i];
 	
 	fft(C, true);
-	for (auto& c : C)
-		c /= n;
+	for (auto& c : C) c /= n;
 
 	return C;
 }
