c5f72463232acb15ce9e2f3789a411d514df62a7
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-06-02 21:16:45 -0300

    Update fast_exp.cpp

--- code/math/fast_exp.cpp
+++ code/math/fast_exp.cpp
@@ -6,10 +6,9 @@ ll fexp(ll x, ll y, ll m) { // iterative
 	ll r = 1;
 	x %= m;
 	while (y > 0) {
-		if (y & 1) 
-			r = (r*x) % m;
-		y >>= 1;
-		x = (x*x) % m;
+		if (y & 1) r = (r * x) % m;
+		y /= 2;
+		x = (x * x) % m;
 	}
 	return r;
 }
