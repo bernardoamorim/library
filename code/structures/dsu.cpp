f799663b5990f4b0d22c6be0f0b6efe30c8b51d9
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-06-02 21:02:43 -0300

    Update dsu.cpp

--- code/structures/dsu.cpp
+++ code/structures/dsu.cpp
@@ -7,20 +7,14 @@
 struct Dsu {
 	vector<int> id, sz;
 	
-	Dsu(int sz_) : id(sz_), sz(sz_, 1) { 
-		iota(id.begin(), id.end(), 0); 
-	}
+	Dsu(int sz_) : id(sz_), sz(sz_, 1) { iota(id.begin(), id.end(), 0); }
 	
-	int find(int a) { 
-		return id[a] = a == id[a] ? a : find(id[a]); 
-	}
+	int find(int a) { return id[a] = a == id[a] ? a : find(id[a]); }
 	
 	void unite(int a, int b) {
 		a = find(a), b = find(b);
-		if (a == b) 
-			return;
-		if (sz[a] < sz[b]) 
-			swap(a, b);
+		if (a == b) return;
+		if (sz[a] < sz[b]) swap(a, b);
 		sz[a] += sz[b], id[b] = a;
 	}
 };
