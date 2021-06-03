24bf65d2f1b4556b0fba73ab17ebb7b547a78fc6
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-06-02 21:15:29 -0300

    Update trie.cpp

--- code/strings/trie.cpp
+++ code/strings/trie.cpp
@@ -15,16 +15,16 @@ struct trie {
 	vector<int> end, pref;
 	int sigma; char norm;
 	trie(int sigma_=26, char norm_='a') : sigma(sigma_), norm(norm_) {
-		to = {vector<int>(sigma)};
+		to.emplace_back(sigma);
 		end = {0}, pref = {0};
 	}
 	void insert(string s) {
 		int x = 0;
-		for(auto c : s) {
+		for (char c : s) {
 			int &nxt = to[x][c-norm];
-			if(!nxt) {
+			if (not nxt) {
 				nxt = to.size();
-				to.push_back(vector<int>(sigma));
+				to.emplace_back(sigma); // yet to be tested
 				end.push_back(0), pref.push_back(0);
 			}
 			x = nxt, pref[x]++;
@@ -33,18 +33,18 @@ struct trie {
 	}
 	void erase(string s) {
 		int x = 0;
-		for(char c : s) {
+		for (char c : s) {
 			int &nxt = to[x][c-norm];
 			x = nxt, pref[x]--;
-			if(!pref[x]) nxt = 0;
+			if (not pref[x]) nxt = 0;
 		}
 		end[x]--;
 	}
 	int find(string s) {
 		int x = 0;
-		for(auto c : s) {
+		for (char c : s) {
 			x = to[x][c-norm];
-			if(!x) return 0;
+			if (not x) return 0;
 		}
 		return x;
 	}
