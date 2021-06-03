20af4f00f53f81a5b69a64b09e0f27e488585211
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-06-02 21:08:05 -0300

    Update kruskal.cpp

--- code/graphs/kruskal.cpp
+++ code/graphs/kruskal.cpp
@@ -17,10 +17,12 @@ pair<ll,vector<t3>> kruskal(int n) {
 	
 	ll cost = 0;
 	vector<t3> mst;
-	for(auto [w,x,y] : edg) if(comps.find(x) != comps.find(y)) {
-		mst.push_back({w,x,y});
-		cost += w;
-		comps.unite(x,y);
+	for (auto [w, x, y] : edg) {
+		if (comps.find(x) != comps.find(y)) {
+			mst.emplace_back(w, x, y);
+			cost += w;
+			comps.unite(x,y);
+		}
 	}
 	return {cost,mst};
 }
