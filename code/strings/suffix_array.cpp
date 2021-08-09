91a4304958e63f032535ce76ccd5930fcc808735
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2021-08-09 20:11:36 -0300

    Update suffix_array.cpp

--- code/strings/suffix_array.cpp
+++ code/strings/suffix_array.cpp
@@ -6,22 +6,22 @@
 
 vector<int> suffix_array(string s) {
 	s += '$';
-	int n = s.size(), N = max(n, 260), ct = 0, k = 0;
+	int n = size(s), N = max(n, 260), ct = 0, k = 0;
 
 	vector<int> p(n), sa(n);
 	
-	for(int i = 0; i < n; i++) sa[i] = i, p[i] = s[i];
+	for (int i = 0; i < n; i++) sa[i] = i, p[i] = s[i];
 	
 	while (k < n) {
 		vector<int> b(N), nsa(n), np(n);
-		for (int i = 0; i < n; i++) b[p[(i-k+n) % n]]++;
-		for (int i = 1; i < N; i++) b[i] += b[i-1];
+		for (int i = 0; i < n; i++) b[p[(i - k + n) % n]]++;
+		for (int i = 1; i < N; i++) b[i] += b[i - 1];
 		
-		for (int i = n-1; i >= 0; i--) 
-			nsa[--b[p[(sa[i]-k+n) % n]]] = (sa[i]-k+n) % n;
+		for (int i = n - 1; i >= 0; i--) 
+			nsa[--b[p[(sa[i] - k + n) % n]]] = (sa[i] - k + n) % n;
 		
 		sa = nsa, ct = 0;
-		pair<int,int> prev, cur;
+		pair<int, int> prev, cur;
 		for (int i = 0; i < n; i++) {
 			cur = {p[sa[i]], p[(sa[i]+k) % n]};
 			if (i and prev != cur) ct++;
@@ -29,20 +29,20 @@ vector<int> suffix_array(string s) {
 		}
 		p = np, k = (k ? 2 * k : 1);
 	}
-	return vector<int>(sa.begin() + 1, sa.end());
+	return vector<int>(begin(sa) + 1, end(sa));
 }
 
 vector<int> lcp(string s, vector<int> sa) {
-	int n = s.size(), k = 0;
+	int n = size(s), k = 0;
 	vector<int> p(n), lcp(n);
 	for (int i = 0; i < n; i++) 
 		p[sa[i]] = i;
 	for (int i = 0; i < n - 1; i++) {
 		if (p[i]) {
-			int j = sa[p[i]-1];
-			while (s[i+k] == s[j+k]) k++;
-			lcp[p[i]] = k, k = max(k-1, 0);
+			int j = sa[p[i] - 1];
+			while (s[i + k] == s[j + k]) k++;
+			lcp[p[i]] = k, k = max(k - 1, 0);
 		}
 	}
-	return vector<int>(lcp.begin()+1, lcp.end());
+	return vector<int>(begin(lcp)+1, end(lcp));
 }
